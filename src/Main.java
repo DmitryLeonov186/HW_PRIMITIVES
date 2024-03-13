0389628b08c6a348c21a300af2eca357c8458def
Dmitry Leonov <lemomonster@mail.ru>
2024-03-13 17:15:42 +0500

    Ready

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 9548;
+        // Цена билета
+        int bonusMile = 20;
+        // Рублей за одну бонусную милю
+        int totalBonus = price/20;
+        // Итого бонусных миль
+
+        System.out.println("Бонус равен " + totalBonus  + " Миль");
+
+
+
+    }
+}
